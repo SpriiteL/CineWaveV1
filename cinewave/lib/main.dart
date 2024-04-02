8ca9277e52e1b4cb15b745e33c0843ccb3cdf932
nathctout ytb <nathanpa.etude@gmail.com>
2024-04-02 11:32:19 +0200

    image depuis firebase storage

--- cinewave/lib/main.dart
+++ cinewave/lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'appbar.dart';
 import 'appbarbottom.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cinewave/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
@@ -19,10 +26,46 @@ class MainApp extends StatelessWidget {
 class MainBody extends StatelessWidget {
   const MainBody({Key? key}) : super(key: key);
 
+  Future<List<String>> getImageUrls() async {
+    // List of image references
+    final List<String> imageRefs = ['Nw6RhJkK_400x400.jpg', 'téléchargement.jpg', 'f4c53422-b80c-4672-bb76-624721ac2d20.jpg', 'Iqoo.png'];
+
+    // Get download URLs for each image
+    final List<String> imageUrls = [];
+    for (var ref in imageRefs) {
+      final url = await FirebaseStorage.instance.ref(ref).getDownloadURL();
+      imageUrls.add(url);
+    }
+
+    return imageUrls;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Hello World!'),
+    return FutureBuilder<List<String>>(
+      future: getImageUrls(),
+      builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        } else {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            // Display images with padding
+            return Row(
+              children: snapshot.data!.map((url) {
+                return Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: 
+                    Image.network(url, fit: BoxFit.cover),
+                  ),
+                );
+              }).toList(),
+            );
+          }
+        }
+      },
     );
   }
 }
